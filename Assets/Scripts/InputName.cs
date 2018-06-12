57b1b60efe204c1c5da910f284b5c9458aebb239
henry33828 <henry33828@users.noreply.github.com>
2018-06-12 15:12:33 +0800

    Initial commit

---
+++ Assets/Scripts/InputName.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class InputName : MonoBehaviour {
+
+    public Text PlayerLastName;
+    public Text PlayerFirstName;
+
+    public void EnterPlayerLastName(Text enterLastName)
+    {
+        PlayerLastName.text = enterLastName.text;
+    }
+
+    public void EnterPlayerFirstName(Text enterFirstName)
+    {
+        PlayerFirstName.text = enterFirstName.text;
+    }
+
+    public void Click() {
+        SceneManager.LoadScene("Room");
+    }
+}
